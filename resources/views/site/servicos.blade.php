f2fe87373937e61e4870a153c555ef05cb80aba6
wbruxy <brunaa0407@gmail.com>
2023-05-16 21:31:06 -0300

    alterações 1605

--- resources/views/site/servicos.blade.php
+++ resources/views/site/servicos.blade.php
@@ -5,7 +5,7 @@
     <!-- Page Header Start -->
     <div class="container-fluid page-header-servicos wow fadeIn" data-wow-delay="0.1s">
         <div class="container text-center">
-            <h1 class="display-3 mb-3 text-white animated slideInDown">Nossos Serviços</h1>
+            <h1 class="display-3 mb-3 text-white animated slideInDown">Outros Serviços</h1>
             <nav aria-label="breadcrumb animated slideInDown">
                 <ol class="breadcrumb justify-content-center mb-0">
                     <li class="breadcrumb-item"><a class="text-body" href="{{route('site.index')}}">Menu</a></li>
